18a1b0897eb59742ef73d4f9a7fa217d1650ceb1
Rohan Bhagat <bhagat1.rohan@gmail.com>
2023-08-03 14:01:31 -0700

    password updated

--- password.py
+++ password.py
@@ -1,30 +1,39 @@
 """Code to generate a password with user preferences"""
 import random
+from random import choice   
 import string
 
 length = input("How long do you want your password to be? ")
-numbers = input("How many numbers do you want? ")
-specialChar = input("How many special characters do you want(ex: {, ], !, }, etc)? ")
-numbers= int(numbers)
-specialChar= int(specialChar)
+numbers = input("Max amount of numbers in your password? ")
+specialChar = input("Max amount of special characters you want(ex: {, ], !, }, etc)? ")
+int_numbers= int(numbers)
+int_specialChar= int(specialChar)
 password = ""
+
 
 for i in range(int(length)):
     randomLetter = random.choice(string.ascii_letters)
-    num = random.randint(0,10)
-    if(num%4==0):
-        randomLetter= randomLetter.upper()
 
-    if (numbers !=0):
-        numbers-=1
-        digits = random.choice(string.digits)
+    num = random.randint(0,100)
 
-    if(specialChar!=0):
-        specialChar-=1
-        chars = random.choice(string.punctuation)
+    if(num %2==0):
+        password += randomLetter
 
-    
-    password += randomLetter
-    
+    if (int_numbers !=0):
+        if(num%6==0):
+                int_numbers-=1
+                digits = random.choice(string.digits)
+                password += str(digits)
+
+    elif (int_specialChar!=0):
+        if(num%6==0):
+            int_specialChar-=1
+            chars = random.choice(string.punctuation)
+            password+=chars
+
+
+while len(password) < int(length):
+    randomLetter = random.choice(string.ascii_letters)
+    password += randomLetter.lower()
 
 print(password)
